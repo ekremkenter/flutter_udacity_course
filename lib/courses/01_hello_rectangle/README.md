256d50723019faec51b3be3487764a6c0d3855b2
Mustafa Ekrem KENTER <mkenter@thy.com>
2018-11-23 19:54:37 +0300

    01 - Hello rectangle implementation

---
+++ lib/courses/01_hello_rectangle/README.md
@@ -0,0 +1,8 @@
+## Specs
+ - App has a title.
+ - App Bar has a title.
+ - Colored rectangle is centered in the app.
+ - Text is centered in the Container widget, and center-aligned.
+
+## Screenshots
+<img src='ios.png' width='350'><img src='android.png' width='350'>
---
+++ lib/courses/01_hello_rectangle/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('01 - Hello Rectangle'),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.greenAccent,
+          height: 400.0,
+          width: 300.0,
+          child: Center(
+            child: Text(
+              'Hello!',
+              style: TextStyle(fontSize: 40.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
